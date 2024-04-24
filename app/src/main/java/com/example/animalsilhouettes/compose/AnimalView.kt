aa226c9521508aedbe9a530903d40717713c4129
marimokomoko <mari_n17@yahoo.co.jp>
2024-04-25 00:19:00 +0900

    [update]レイアウト変更

--- app/src/main/java/com/example/animalsilhouettes/compose/AnimalView.kt
+++ app/src/main/java/com/example/animalsilhouettes/compose/AnimalView.kt
@@ -2,13 +2,18 @@ package com.example.animalsilhouettes.compose
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,7 +24,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.animalsilhouettes.R
 
 @Composable
@@ -29,7 +36,6 @@ fun AnimalView(
     imgRId: Int,
 ) {
     var isVisible by remember { mutableStateOf(false) }
-
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -37,26 +43,29 @@ fun AnimalView(
             painter = painterResource(id = imgRId),
             contentDescription = imgName,
             modifier = Modifier
+                .fillMaxWidth()
                 .size(250.dp)
                 .padding(8.dp)
         )
         Spacer(modifier = Modifier.height(20.dp))
-
         Button(
             onClick = { isVisible = !isVisible },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.DarkGray,
-                contentColor = Color.White,
-            ),
-//            colors = ButtonDefaults.buttonColors(
-//                backgroundColor = if (isVisible) Color.Green else Color.Red
-//            ),
-            shape = RoundedCornerShape(8.dp)
+            contentPadding = PaddingValues(
+                start = 20.dp,
+                top = 12.dp,
+                end = 20.dp,
+                bottom = 12.dp
+            )
         ) {
-            Text("？？？")
+            Icon(
+                Icons.Filled.KeyboardArrowDown,
+                contentDescription = "KeyboardArrowDown",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text("タップ")
         }
-
-
+        Spacer(modifier = Modifier.height(20.dp))
         val painterId = when (imgName) {
             "cat" -> R.drawable.cat
             "chicken" -> R.drawable.chicken
@@ -68,15 +77,22 @@ fun AnimalView(
             "pig" -> R.drawable.pig
             "turtle" -> R.drawable.turtle
             "rabbit" -> R.drawable.rabbit
-            else -> R.drawable.cat // デフォルトの画像
+            else -> null
         }
-        if (isVisible) {
+        if (isVisible && painterId != null) {
             Image(
                 painter = painterResource(id = painterId),
-                contentDescription = "あとで",
+                contentDescription = "",
                 modifier = Modifier
-                    .size(150.dp) // 画像のサイズを設定
-                    .padding(8.dp) // 余白を追加
+                    .size(250.dp)
+                    .padding(8.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = imgName,
+                color = Color.DarkGray,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.ExtraBold,
             )
         }
     }
--- app/src/main/java/com/example/animalsilhouettes/compose/SilhouettrsImage.kt
+++ app/src/main/java/com/example/animalsilhouettes/compose/SilhouettrsImage.kt
@@ -2,7 +2,6 @@ package com.example.animalsilhouettes.compose
 
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
@@ -13,37 +12,34 @@ import com.example.animalsilhouettes.R
 
 @Composable
 fun SilhouettrsImage() {
-    Column {
-        Box(contentAlignment = Alignment.TopCenter) {
-            val scrollState = rememberScrollState()
-            val imgItems = listOf(
-                Pair("cat", R.drawable.cat_si),
-                Pair("chicken", R.drawable.chicken_si),
-                Pair("crocodile", R.drawable.crocodile_si),
-                Pair("hedgehog", R.drawable.hedgehog_si),
-                Pair("koala", R.drawable.koala_si),
-                Pair("lion", R.drawable.lion_si),
-                Pair("owl", R.drawable.owl_si),
-                Pair("pig", R.drawable.pig_si),
-                Pair("turtle", R.drawable.turtle_si),
-                Pair("rabbit", R.drawable.rabbit_si)
-            )
-            println(imgItems)
-            Row(
-                modifier = Modifier.horizontalScroll(scrollState)
-            ) {
-                imgItems.forEach { (imgName, imgResourceId) ->
-                    AnimalView(
-                        id = imgName.length,
-                        imgName = imgName,
-                        imgRId = imgResourceId
-                    )
-                }
+    Box(contentAlignment = Alignment.TopCenter) {
+        val scrollState = rememberScrollState()
+        val imgItems = listOf(
+            Pair("cat", R.drawable.cat_si),
+            Pair("chicken", R.drawable.chicken_si),
+            Pair("crocodile", R.drawable.crocodile_si),
+            Pair("hedgehog", R.drawable.hedgehog_si),
+            Pair("koala", R.drawable.koala_si),
+            Pair("lion", R.drawable.lion_si),
+            Pair("owl", R.drawable.owl_si),
+            Pair("pig", R.drawable.pig_si),
+            Pair("turtle", R.drawable.turtle_si),
+            Pair("rabbit", R.drawable.rabbit_si)
+        )
+        println(imgItems)
+        Row(
+            modifier = Modifier.horizontalScroll(scrollState)
+        ) {
+            imgItems.forEach { (imgName, imgResourceId) ->
+                AnimalView(
+                    id = imgName.length,
+                    imgName = imgName,
+                    imgRId = imgResourceId
+                )
             }
         }
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 private fun SilhouettrsImagePre() {
