8d8aaa0fff42a918eb71c84bc43bd0f77ac71107
marimokomoko <mari_n17@yahoo.co.jp>
2024-04-25 00:19:28 +0900

    [update]文字列追加

--- app/src/main/java/com/example/animalsilhouettes/MainActivity.kt
+++ app/src/main/java/com/example/animalsilhouettes/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -15,7 +16,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,12 +46,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent() {
-    // シークレット表示
-//    SilhouettesImage()
-    // 動物表示
-    SilhouettrsImage()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "あにまるシルエット",
+            color = Color.DarkGray,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.ExtraBold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = "横にスクロールしてみよう",
+            color = Color.LightGray,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.ExtraBold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        SilhouettrsImage()
+    }
 }
-
 @Preview(showBackground = true)
 @Composable
 private fun MainContentPre() {
