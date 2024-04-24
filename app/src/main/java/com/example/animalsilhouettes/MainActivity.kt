b720b07178965efcf3ee2f1112de3162cf90a4a1
marimokomoko <mari_n17@yahoo.co.jp>
2024-04-24 15:08:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/animalsilhouettes/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.animalsilhouettes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.GenericShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.animalsilhouettes.compose.AnimalCard
+import com.example.animalsilhouettes.ui.theme.AnimalSilhouettesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimalSilhouettesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    val buttonEnable by remember { mutableStateOf(true) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text(
+            text = "Animal Silhouette",
+            color = Color.DarkGray,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.ExtraBold,
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.cat_si),
+            contentDescription = "profile",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(10.dp))
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // 詳細表示ボタン
+        var IsShowDetail by remember { mutableStateOf(false) }
+        Button(
+            modifier = Modifier.padding(4.dp),
+            enabled = buttonEnable,
+            onClick = { IsShowDetail = !IsShowDetail },
+            shape = GenericShape { size, layoutDirection ->
+                moveTo(size.width / 2f, 0f)
+                lineTo(size.width, size.height)
+                lineTo(0f, size.height)
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.DarkGray,
+                contentColor = Color.White,
+            )
+        ) {
+            Text("？？？")
+        }
+        Text(
+            text = "↑ Let's tap",
+            color = Color.DarkGray,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.ExtraBold,
+        )
+        if (IsShowDetail) {
+            AnimalCard()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainContentPre() {
+    MainContent()
+}
---
+++ app/src/main/java/com/example/animalsilhouettes/compose/AnimalCard.kt
@@ -0,0 +1,55 @@
+package com.example.animalsilhouettes.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.animalsilhouettes.R
+
+@Composable
+//fun AnimalCard(animalName: String, imgId: Int) {
+fun AnimalCard() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .width(300.dp)
+            .clip(RoundedCornerShape(30.dp))
+            .background(Color.LightGray.copy(alpha = 0.5f))
+            .padding(horizontal = 10.dp, vertical = 20.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.cat),
+            contentDescription = "cat",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(10.dp))
+        )
+        Text(
+            text = "ネコちゃん",
+            color = Color.DarkGray,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.ExtraBold,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AnimalCardPre() {
+    AnimalCard()
+}
